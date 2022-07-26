5ef80948cd9abfd87067dd6e756d0d8751acb75b
Facundo Lavagnino <flavagnino8@gmail.com>
2022-07-25 23:31:21 -0300

    some courses added

--- src/checkout/helpers/courses.ts
+++ src/checkout/helpers/courses.ts
@@ -9,6 +9,26 @@ const items = [
       'https://emprendedordigital.com.ar/wp-content/uploads/elementor/thumbs/photo_5051202121206180421_y-removebg-preview-prafwd1fjzn7jmld8o523dftfvba01g33xtqnfckxs.png',
     url: 'https://mega.nz/folder/6CAEhAIT#XrJk-DcB-TlqXMKa_mOxpQ',
   },
+  {
+    id: '2',
+    title: 'Embudo viral de ventas por internet',
+    description:
+      'Aprende a crear un embudo viral de ventas en internet, expande tu negocio y consigue 10 veces mas clientes',
+    price: 2470,
+    picture:
+      'https://media.istockphoto.com/vectors/the-process-of-communication-and-attracting-new-customers-and-making-vector-id1205922607?k=20&m=1205922607&s=612x612&w=0&h=bkzZdM0aj5nig-2HhK74G_LonR2Gw5kSEP2YAQeI1Ys=',
+    url: 'https://mega.nz/folder/ebImiaTL#nEgM5tN9U6Mei1OKtvCA0A',
+  },
+  {
+    id: '3',
+    title: 'Marketing de afiliados SkyRocket',
+    description:
+      'Crea un negocio de marketing de afiliados desde tu casa y genera ingresos ilimitados',
+    price: 2470,
+    picture:
+      'https://cursos-economicos.com/wp-content/uploads/2020/12/23-SkyRocket-Affiliates-Juan-Chaustre.jpg',
+    url: 'https://mega.nz/folder/OOZU3azQ#POhkHTxq1U49Oyq2lfybbQ',
+  },
 ];
 
 export default items;
